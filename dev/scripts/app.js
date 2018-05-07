79fa65bd9999b096af27cd6bcf11e014503a7a64
Nishi Rathod <nishi85@outlook.com>
2018-05-06 22:25:36 -0400

    added popup modal

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -52,6 +52,9 @@ const searchInput = document.getElementById('searchInput');
 
 searchForm.addEventListener('submit', e => {
   e.preventDefault();
+  
+  // When the user clicks on the button, open the modal 
+  modal.style.display = "block";
   //get search term
   const searchTerm = searchInput.value;
 
@@ -66,3 +69,29 @@ searchForm.addEventListener('submit', e => {
     //search API
     getQuote(searchTerm);
 })
+
+// Get the modal
+const modal = document.getElementById('myModal');
+
+// // Get the button that opens the modal
+// const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal 
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
